0b3ac17a8c022b472dbfa3e445a0afc93476c96e
ayaznoori <ayaz.noori15@gmail.com>
2025-01-18 07:23:02 +0530

    Fixed bug

--- model/checkPointSchema.js
+++ model/checkPointSchema.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const checkpointSchema = new mongoose.Schema({
     time: { type: String, unique: true, required: true }, // Ensure 'time' is unique and required
-    teamsResponded: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team',unique: true }],
+    teamsResponded: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team'}],
 });
 module.exports = mongoose.model('Checkpoint', checkpointSchema);
